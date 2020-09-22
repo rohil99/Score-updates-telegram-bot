9e8d81bea7c2c3e6bc2b715b6631b75b7ade0386
Rohil Soni <71592839+rohil99@users.noreply.github.com>
2020-09-22 15:45:24 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,31 @@
+import telebot
+from bs4 import BeautifulSoup
+import requests
+from pprint import pprint
+bot=telebot.TeleBot(ENTER YOUR BOT KEY HERE)
+
+
+@bot.message_handler(commands=['getscore','start'])
+def greeting_message(message):
+    bot.reply_to(message, "Here u can get the IPL score updates\n\nType /score to get current match score updates")
+
+@bot.message_handler(commands=['score'])
+def send_score(message):
+    l=[]
+    url='http://static.cricinfo.com/rss/livescores.xml'
+    page=requests.get(url)
+    soup=BeautifulSoup(page.text, 'html.parser')
+    pprint(soup)
+    result=soup.find_all('description')
+    for match in result:
+        l.append(match)
+    for i in l:
+        bot.reply_to(message, i)
+    
+@bot.message_handler(func=lambda message:True)
+def send_default(message):
+    bot.reply_to(message, 'please enter the correct command')
+
+bot.polling()
+
+
